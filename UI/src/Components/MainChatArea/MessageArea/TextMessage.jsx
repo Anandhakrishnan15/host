db3318d3b8c4cc2fc9ef79a4faee06d2a931accf
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2024-03-23 11:09:18 +0530

    message scroll down effect

--- UI/src/Components/MainChatArea/MessageArea/TextMessage.jsx
+++ UI/src/Components/MainChatArea/MessageArea/TextMessage.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GetUsersMessages from "./GetUsersMessages";
 import Message from "./Message";
 import "./MessageClouse.css";
 
 const TextMessage = () => {
   const { messages, lodaing } = GetUsersMessages();
+  const scrollDown = useRef()
   console.log(messages);
+
+  useEffect(()=>{
+    scrollDown.current?.scrollIntoView({behavior:'smooth'})
+  },[messages])
  
   return (
     <div className="messageShowArea">
       {/* if not loading and the mesage lengsth is largrer than a ) and map it and show the message componant */}
       {!lodaing && messages.length >0 && messages.map((message) =>(
-        <Message key={message._id} messages={message}/>
+        <div key={message._id} ref={scrollDown}>
+        <Message  messages={message}/>
+        </div>
       )) }
       {/* if the loading is true then show the loading  */}
      {lodaing ? (<p>loading</p>) : ""}
